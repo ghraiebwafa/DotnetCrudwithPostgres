ea96e3a8cd94dac39509de2d72722ebe0da21bd7
wafa <wafaaghraieb@gmail.com>
2024-11-14 16:42:35 +0100

    Initial commit

---
+++ Models/MydbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserApi.Models;
+
+public partial class MydbContext : DbContext
+{
+    private readonly IConfiguration? _configuration;
+
+    public MydbContext()
+    {
+    }
+
+    public MydbContext(DbContextOptions<MydbContext> options,IConfiguration configuration
+    )
+        : base(options)
+    {
+        _configuration = configuration;
+
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Use the connection string from configuration
+        var connectionString = _configuration?.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "users_email_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
